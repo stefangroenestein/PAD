210f6ec933f4db7761ba5630e00656e43f477b75
Stefan Groenestein <stefan@stefgroenestein.nl>
2016-03-29 14:38:58 +0200

    first

---
+++ src/main/java/phidgetspad/KitConnector.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phidgetspad;
+
+/**
+ *
+ * @author <Stefan Groenestein, IT102, 500726588>
+ */
+public class KitConnector {
+    
+    
+    
+}

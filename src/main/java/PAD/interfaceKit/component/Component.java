1458f6da988d11fb6acab1a081b606ad766aa0c9
YouriG <youridudock@hotmail.com>
2016-04-26 11:18:37 +0200

    Ids fix

--- src/main/java/PAD/interfaceKit/component/Component.java
+++ src/main/java/PAD/interfaceKit/component/Component.java
@@ -20,9 +20,18 @@ public enum Component {
     
     BUTTON_GREEN(0, new GreenButtonAction()),
     
-    PRESSURE_PLATE_ONE(1, new PressurePlateAction(LinkedComponent.ONE)),
+    PRESSURE_PLATE_ONE(3, new PressurePlateAction(LinkedComponent.ONE)),
+    PRESSURE_PLATE_TWO(4, new PressurePlateAction(LinkedComponent.TWO)),
+    PRESSURE_PLATE_THREE(5, new PressurePlateAction(LinkedComponent.THREE)),
+    PRESSURE_PLATE_FOUR(6, new PressurePlateAction(LinkedComponent.FOUR)),
+    PRESSURE_PLATE_FIVE(7, new PressurePlateAction(LinkedComponent.FIVE)),
     
-    MAGNET_ONE(6, new MagnetAction());
+    MAGNET_ONE(0, new MagnetAction()),
+    MAGNET_TWO(1, new MagnetAction()),
+    MAGNET_THREE(2, new MagnetAction()),
+    MAGNET_FOUR(3, new MagnetAction()),
+    MAGNET_FIVE(4, new MagnetAction());
+    
     
     private int id;
     
--- src/main/java/PAD/interfaceKit/component/LinkedComponent.java
+++ src/main/java/PAD/interfaceKit/component/LinkedComponent.java
@@ -12,7 +12,11 @@ package PAD.interfaceKit.component;
  */
 public enum LinkedComponent {
     
-    ONE(Component.MAGNET_ONE, Component.PRESSURE_PLATE_ONE);
+    ONE(Component.MAGNET_ONE, Component.PRESSURE_PLATE_ONE),
+    TWO(Component.MAGNET_TWO, Component.PRESSURE_PLATE_TWO),
+    THREE(Component.MAGNET_THREE, Component.PRESSURE_PLATE_THREE),
+    FOUR(Component.MAGNET_FOUR, Component.PRESSURE_PLATE_FOUR),
+    FIVE(Component.MAGNET_FIVE, Component.PRESSURE_PLATE_FIVE);
     
     private Component magnet;
     
